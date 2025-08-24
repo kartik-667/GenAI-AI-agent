4d2bfdc443121f6726a8f1a893d4120d4892dad7
Kartik Verma <vermakartik667@gmail.com>
2025-08-24 23:01:51 +0530

    added a llm model with context too

---
+++ index.js
@@ -0,0 +1,59 @@
+import env from 'dotenv'
+env.config()
+import readlinesync from 'readline-sync'
+
+
+import Groq from "groq-sdk";
+const groq = new Groq({ apiKey: process.env.APIKEY });
+
+let context=[]
+
+
+async function main(qn) {
+    try {
+        context.push({
+            role:"user",
+            content:qn
+        })
+        
+        const completion = await groq.chat.completions
+          .create({
+            messages:context,
+            model: "openai/gpt-oss-20b",
+          })
+          .then((chatCompletion) => {
+            let response=chatCompletion.choices[0]?.message?.content
+            console.log(response || "");
+            context.push({
+                role:"assistant",
+                content:response
+            })
+
+            
+          });
+        
+    } catch (error) {
+        console.log("some error occured ",error);
+        
+        
+    }
+}
+
+async function callModel(){
+    while(true){
+        let qn=readlinesync.question("Enter your qn -> ")
+        if(qn.trim().toLowerCase()==="exit"){
+            return
+        }
+    
+        await main(qn)
+
+    }
+
+
+}
+callModel()
+
+
+const key=process.env.APIKEY
+// console.log(key);
